18e5f9d46a279a2e4d5b0b67ce48739328537ba7
hewmx <mxhew20@gmail.com>
2023-06-12 12:57:42 +1200

    Fixed some text

--- app/src/main/java/com/example/assignment2quiz/LoginActivity.java
+++ app/src/main/java/com/example/assignment2quiz/LoginActivity.java
@@ -92,7 +92,6 @@ public class LoginActivity extends AppCompatActivity {
         }
         Handler.setCurrentUser(user);
         startActivity(loginIntent);
-
     }
 
     private void loadRegistration() {
